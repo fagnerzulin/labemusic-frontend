155f19922de5ccd2b58758856b9c58dad13d4dd0
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-10 18:04:30 -0300

    Create SignUpModal styled

---
+++ src/components/SignUpModal/signUpModal.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ContainerFormSingUp = styled.form`
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const SubTitle = styled.h2`
+  font-weight: bold;
+  font-size: 1.6em;
+`;
