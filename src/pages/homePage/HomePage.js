f01b880e2b13d88b22577a882ab196f50fa140bd
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-10 17:05:53 -0300

    Done HomePage

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,9 +1,33 @@
+import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import MainContainer from '../../components/StyledComponents/MainContainer.styled';
+import palette from '../../constants/paletteColor';
+import { goToLoginPage } from '../../routers/coordinate';
+import { Box, Subtitle, Title } from './homePage.styled';
 
 export default function HomePage() {
+  const history = useHistory();
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <MainContainer>
+      <Box>
+        <Title>LabeMusic</Title>
+        <Subtitle>🎶 Aqui sua música acontece 🎵</Subtitle>
+        <Button
+          bg={palette.red}
+          _active={{
+            transform: 'scale(0.85)',
+          }}
+          _hover={{
+            background: '#702E2E',
+          }}
+          color="white"
+          size="lg"
+          onClick={() => goToLoginPage(history)}
+        >
+          Login
+        </Button>
+      </Box>
+    </MainContainer>
   );
 }
