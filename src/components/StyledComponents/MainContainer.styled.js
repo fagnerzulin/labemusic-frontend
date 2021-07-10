f995594cb3ad37ca43bcc6d9e99887144edc3b3a
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-10 16:56:04 -0300

    Create MainContainer

---
+++ src/components/StyledComponents/MainContainer.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export default MainContainer;
