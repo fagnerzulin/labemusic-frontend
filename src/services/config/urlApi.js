bb9907fc01c010b89288fb5ebe6b4ba794025992
Fagner Zulin <fagnerzulin11@gmail.com>
2021-07-12 15:33:35 -0300

    Fix url to musics

--- src/services/config/urlApi.js
+++ src/services/config/urlApi.js
@@ -1,4 +1,4 @@
 export const userUrl = 'https://labemusic-backend.herokuapp.com/user';
 export const albumUrl = 'https://labemusic-backend.herokuapp.com/album';
 export const genreUrl = 'https://labemusic-backend.herokuapp.com/genre';
-export const musicUrl = 'https://labemusic-backend.herokuapp.com/user';
+export const musicUrl = 'https://labemusic-backend.herokuapp.com/music';
